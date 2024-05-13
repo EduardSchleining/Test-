10afb8ceeed1d82a5ab80409b0b5a1f0710f37e4
Eduard Schleining <edi.schleining@web.de>
2024-05-13 12:30:32 +0200

    x

--- src/main/java/com/example/Test/TestController.java
+++ src/main/java/com/example/Test/TestController.java
@@ -1,17 +1,22 @@
 package com.example.test;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-	
+
+	ExampleService eS = new ExampleService();
+
 	@GetMapping("/")
 	public String index() {
 		System.out.println("GET Request erhalten!");
-        return "Response für GET Request";
+
+		String securePassword = eS.passwordEncryption("geheim");
+
+		System.out.println(securePassword);
+
+		return "Response für GET Request";
 	}
-	
 
 }
